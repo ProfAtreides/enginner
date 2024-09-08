33058287e4db60b587c97dc9c848f06d72414337
ProfAtreides <dambrozewicz7@gmail.com>
2024-09-08 03:27:29 +0200

    Fixed issue with using unreliable library in translation.py, moving onto using speech diarization as a good way to divide a dialogue into sentences

--- translation.py
+++ translation.py
@@ -1,7 +1,14 @@
-import translators as ts
+from deep_translator import DeeplTranslator
+import pandas as pd
+
+# Load API token
+df = pd.read_csv('api_token.csv')
+api_key = df['deepl'].iloc[0]
+
+print(api_key)
 
 print("Start translating...")
 
 to_translate = open("output.txt", "r").read()
 
-print(ts.translate_text(to_translate, "baidu", "en", "pl"))
+print(DeeplTranslator(api_key=api_key, source='english', target='polish').translate(to_translate))
